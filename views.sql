9ff1755781d4a81c978b88449fcc2c351517092e
Lorenz Koestler (lko) <lorenz.koestler@datahouse.ch>
2015-12-25 16:33:24 +0100

    remove length_mm from article table, create v_instance and v_quarter_tot view

--- views.sql
+++ views.sql
@@ -22,7 +22,6 @@ SELECT
     SEPARATOR ',\n'
   ) AS col_code,
   numb_strings,
-  length_mm,
   foto_ma,
   foto_mi,
   foto_prod,
@@ -56,3 +55,23 @@ SELECT material_id, CONCAT(p.abbr, vendor_ref) col_code
 FROM product p
 JOIN material m USING(product_id)
 ORDER BY col_code ASC;
+
+DROP VIEW IF EXISTS v_instance;
+CREATE VIEW v_instance AS
+SELECT instance_id, article_id, i.user_id creator_uid, length_mm, price_cchf, deduction, order_id, o.platform_id, o.user_id buyer_uid, o.created order_created, o.paid order_paid, o.sent order_sent
+FROM `instance` i
+LEFT JOIN order_x_instance USING(instance_id)
+LEFT JOIN `order` o USING(order_id);
+
+DROP VIEW IF EXISTS v_quarter_tot;
+CREATE VIEW v_quarter_tot AS
+SELECT
+  YEAR(o.paid) y, QUARTER(o.paid) q,
+  i.user_id creator_uid,
+  COUNT(i.instance_id) cnt_instances,
+  SUM(i.price_cchf * (1-i.deduction/100))/100 price_chf
+FROM instance i
+JOIN order_x_instance USING(instance_id)
+JOIN `order` o USING(order_id)
+WHERE o.paid IS NOT NULL
+GROUP BY y, q, creator_uid WITH ROLLUP;
